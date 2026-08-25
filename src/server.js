b418f9ff13b3be250981ba11580061d58dfae1b8
muzammilriazofficial <muzamilriazthaheemmrt@gmail.com>
2026-08-26 00:18:45 +0500

    fix: add CORS for frontend Vercel URL

--- src/server.js
+++ src/server.js
@@ -10,7 +10,12 @@ connectDB();
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: ["https://project-f-peach.vercel.app", "http://localhost:3000"],
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
