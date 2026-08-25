1296d3bd5e2143783eff4018690208d166eabd1f
muzammilriazofficial <muzamilriazthaheemmrt@gmail.com>
2026-08-26 00:57:41 +0500

    fix: MongoDB connection caching for Vercel serverless

--- api/index.js
+++ api/index.js
@@ -1,5 +1,7 @@
 const app = require("../src/server");
+const connectDB = require("../src/config/db");
 
 module.exports = async function handler(req, res) {
+  await connectDB();
   return app(req, res);
 };
